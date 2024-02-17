d5f4d2ba0bfcaa0174ff9b07a23031dfcc29a1f1
Tanishq Mishra <mishratanishq619@gmail.com>
2024-02-17 17:10:33 +0530

    techno-frontend init

---
+++ techno_frontend/src/app/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+	title: "Technorollix_2024",
+	description: "by CodeForIt team.",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
+}
