d9313a8093a40ac1d0a5bbc0d0b3dfd7b653ef3b
Tanishq Mishra <mishratanishq619@gmail.com>
2024-02-18 23:35:04 +0530

    18/2/23:1

--- techno_frontend/src/app/events/page.js
+++ techno_frontend/src/app/events/page.js
@@ -1,19 +1,25 @@
 import React from "react";
 import EventCard from "@/components/EventCard";
 const page = () => {
-  return (
-    <div className="bg-black bg-cover bg-center flex flex-wrap pl-20">
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-      <EventCard />
-    </div>
-  );
+	return (
+		<div className="w-full h-screen py-24 bg-cover  bg-center bg-[url('/mainbg.jpg')]  flex flex-wrap gap-10 items-center justify-evenly overflow-scroll">
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			{/* <EventCard /> */}
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+			<EventCard />
+		</div>
+	);
 };
 
 export default page;
--- techno_frontend/src/app/layout.js
+++ techno_frontend/src/app/layout.js
@@ -1,23 +1,21 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Home from "@/components/Home";
 import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Technorollix_2024",
-  description: "by CodeForIt team.",
+	title: "Technorollix_2024",
+	description: "by CodeForIt team.",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Header />
+				{children}
+			</body>
+		</html>
+	);
 }
