d1a66fbfd16ff3347da3d646ca96491a25454e75
Tanishq Mishra <mishratanishq619@gmail.com>
2024-02-19 12:22:07 +0530

    19/2/24:3-searchParams

--- techno_frontend/src/app/registration/next/page.js
+++ techno_frontend/src/app/registration/next/page.js
@@ -1,11 +1,12 @@
 "use client";
 import EventCard from "@/components/EventCard";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 function page() {
-	const router = useRouter();
-	// console.log(router.query);
+	const searchParams = useSearchParams();
+	// console.log(searchParams.get("emailRef"));
+	const emailRef = searchParams.get("emailRef"); // required email.
 	return (
 		<div className="w-full h-screen py-24 bg-cover  bg-center bg-[url('/mainbg.jpg')] flex flex-col items-center justify-center">
 			<p className="text-4xl text-white font-bold">EventsCheckbox</p>
