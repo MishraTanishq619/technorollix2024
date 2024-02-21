8be95f74bbe0396bb8ea577c5e5190538947a01d
Tanishq Mishra <mishratanishq619@gmail.com>
2024-02-21 18:14:49 +0530

    Legacy-page

--- techno_frontend/src/app/layout.js
+++ techno_frontend/src/app/layout.js
@@ -1,9 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const metadata = {
 	title: "Technorollix_2024",
@@ -12,12 +10,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-<GoogleOAuthProvider clientId="836755220699-ojv84oup2nfa7li643q0jssqiftnk6o5.apps.googleusercontent.com">
-		<html lang="en">
-			<body className="w-full h-screen py-16 bg-cover bg-fixed bg-no-repeat bg-center bg-[url('/mainbg.jpg')]  flex flex-wrap gap-0 items-center justify-evenly overflow-y-scroll">
-				<Header />
-				{children}
-			</body>
-		</html></GoogleOAuthProvider>
+		<GoogleOAuthProvider clientId="836755220699-ojv84oup2nfa7li643q0jssqiftnk6o5.apps.googleusercontent.com">
+			<html lang="en">
+				<body className="w-full h-screen py-16 bg-cover bg-fixed bg-no-repeat bg-center bg-[url('/mainbg.jpg')]  flex flex-wrap gap-0 items-center overflow-y-scroll">
+					<Header />
+					{children}
+				</body>
+			</html>
+		</GoogleOAuthProvider>
 	);
 }
