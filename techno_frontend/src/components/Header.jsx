6134097295fa3803925aaeaf3407ce225b9df7d9
Tanishq Mishra <mishratanishq619@gmail.com>
2024-02-22 12:23:47 +0530

    header-styles

--- techno_frontend/src/components/Header.jsx
+++ techno_frontend/src/components/Header.jsx
@@ -5,47 +5,53 @@ import { CiMenuFries } from "react-icons/ci";
 // import {  } from '@react-oauth/google';
 
 function Header() {
-  return (
-    <>
-      <div
-        id="div-conatiner"
-        className=" bg-[#000] bg-opacity-40 grid grid-cols-8 text-2xl fixed z-10 top-0 w-full left-0 pt-2 text-white"
-      >
-        <a href="/" className="transition-transform transform hover:scale-105">
-          <img src="/logo.png " className="col-span-2 pb-4"></img>
-        </a>
+	return (
+		<>
+			<div
+				id="div-conatiner"
+				className=" bg-[#000] bg-opacity-40 flex justify-between  text-2xl fixed z-10 top-0 w-full left-0 pt-2 text-white"
+			>
+				<div className="flex justify-around w-[35%]">
+					<a
+						href="/"
+						className="transition-transform transform hover:scale-105"
+					>
+						<img src="/logo.png " className=" pb-4"></img>
+					</a>
+				</div>
 
-        <span id="space" className="col-span-1 "></span>
-        <a
-          href="/about"
-          className="flex items-center justify-end col-span-1 transition-transform transform hover:scale-105"
-        >
-          ABOUT
-        </a>
-        <a
-          href="/legacy"
-          className="flex items-center justify-end col-span-1 transition-transform transform hover:scale-105"
-        >
-          LEGACY
-        </a>
-        <a
-          href="/events"
-          className="flex items-center justify-end col-span-1 transition-transform transform hover:scale-105"
-        >
-          EVENTS
-        </a>
-        <a
-          href="/contact"
-          className="flex items-center justify-end col-span-1 transition-transform transform hover:scale-105"
-        >
-          CONTACT
-        </a>
-        <div className="flex items-center justify-end pr-6 col-span-1 transition-transform transform hover:scale-105">
-          <CiMenuFries />
-        </div>
-      </div>
-    </>
-  );
+				<div className="flex justify-around w-[60%]">
+					<a
+						href="/about"
+						className="flex items-center justify-end  transition-transform transform hover:scale-105"
+					>
+						ABOUT
+					</a>
+					<a
+						href="/legacy"
+						className="flex items-center justify-end  transition-transform transform hover:scale-105"
+					>
+						LEGACY
+					</a>
+					<a
+						href="/events"
+						className="flex items-center justify-end  transition-transform transform hover:scale-105"
+					>
+						EVENTS
+					</a>
+					<a
+						href="/contact"
+						className="flex items-center justify-end  transition-transform transform hover:scale-105"
+					>
+						CONTACT
+					</a>
+					<div className="flex items-center justify-end pr-6  transition-transform transform hover:scale-105">
+						<CiMenuFries />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Header;
