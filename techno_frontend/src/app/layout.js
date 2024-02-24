8741baec584691e9b9bdf01cadd4d57536b38d81
ShobhitPatra <shobhit9999999@gmail.com>
2024-02-24 11:00:16 +0530

    neon text countdown

--- techno_frontend/src/app/layout.js
+++ techno_frontend/src/app/layout.js
@@ -3,19 +3,19 @@ import "./globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const metadata = {
-	title: "Technorollix_2024",
-	description: "by CodeForIt team.",
+  title: "Technorollix_2024",
+  description: "by CodeForIt team.",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<GoogleOAuthProvider clientId="836755220699-ojv84oup2nfa7li643q0jssqiftnk6o5.apps.googleusercontent.com">
-			<html lang="en">
-				<body className="w-full h-full mt-16  bg-cover bg-fixed bg-no-repeat bg-center bg-[url('/star-wars-fight.png')] flex flex-wrap gap-0 items-center overflow-y-scroll">
-				{/* <body className="w-full h-screen pt-16 bg-cover bg-fixed bg-no-repeat bg-center bg-[url('/star-war-blue-bg.png')]  flex flex-wrap gap-0 items-center overflow-y-scroll"> */}
-					{children}
-				</body>
-			</html>
-		</GoogleOAuthProvider>
-	);
+  return (
+    <GoogleOAuthProvider clientId="836755220699-ojv84oup2nfa7li643q0jssqiftnk6o5.apps.googleusercontent.com">
+      <html lang="en">
+        <body className="w-full h-full mt-16  bg-cover bg-fixed bg-no-repeat bg-center bg-[url('/enhanced-image.png')] flex flex-wrap gap-0 items-center overflow-y-scroll">
+          {/* <body className="w-full h-screen pt-16 bg-cover bg-fixed bg-no-repeat bg-center bg-[url('/star-war-blue-bg.png')]  flex flex-wrap gap-0 items-center overflow-y-scroll"> */}
+          {children}
+        </body>
+      </html>
+    </GoogleOAuthProvider>
+  );
 }
--- techno_frontend/src/app/registration/next/eventRegistrationCard.jsx
+++ techno_frontend/src/app/registration/next/eventRegistrationCard.jsx
@@ -1,125 +1,148 @@
-"use client"
-import { useState, useEffect } from 'react';
+"use client";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
 function EventsRegistrationPage() {
-    const [events, setEvents] = useState([]);
-    const [selectedEvents, setSelectedEvents] = useState([]);
-    const [additionalDetails, setAdditionalDetails] = useState([]);
-    const [leaderEmail, setLeaderEmail] = useState('');
-    const [teammateEmails, setTeammateEmails] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [selectedEvents, setSelectedEvents] = useState([]);
+  const [additionalDetails, setAdditionalDetails] = useState([]);
+  const [leaderEmail, setLeaderEmail] = useState("");
+  const [teammateEmails, setTeammateEmails] = useState([]);
 
-    const searchParams = useSearchParams();
-    const emailRef = searchParams.get("emailRef");
+  const searchParams = useSearchParams();
+  const emailRef = searchParams.get("emailRef");
 
-    useEffect(() => {
-        setLeaderEmail(emailRef);
-    }, []);
+  useEffect(() => {
+    setLeaderEmail(emailRef);
+  }, []);
 
-    useEffect(() => {
-        fetch('http://technorollix.opju.ac.in:4000/api/allEvents')
-            .then(response => response.json())
-            .then(data => setEvents(data))
-            .catch(error => console.error('Error fetching events:', error));
-    }, []);
+  useEffect(() => {
+    fetch("http://10.60.200.201:4000/api/allEvents")
+      .then((response) => response.json())
+      .then((data) => setEvents(data))
+      .catch((error) => console.error("Error fetching events:", error));
+  }, []);
 
-    const handleEventCardClick = (eventId) => {
-        const selectedIndex = selectedEvents.indexOf(eventId);
-        const isEventSelected = selectedIndex !== -1;
+  const handleEventCardClick = (eventId) => {
+    const selectedIndex = selectedEvents.indexOf(eventId);
+    const isEventSelected = selectedIndex !== -1;
 
-        if (isEventSelected) {
-            const updatedSelectedEvents = [...selectedEvents];
-            updatedSelectedEvents.splice(selectedIndex, 1);
-            setSelectedEvents(updatedSelectedEvents);
+    if (isEventSelected) {
+      const updatedSelectedEvents = [...selectedEvents];
+      updatedSelectedEvents.splice(selectedIndex, 1);
+      setSelectedEvents(updatedSelectedEvents);
 
-            const updatedAdditionalDetails = [...additionalDetails];
-            updatedAdditionalDetails.splice(selectedIndex, 1);
-            setAdditionalDetails(updatedAdditionalDetails);
-        } else {
-            setSelectedEvents([...selectedEvents, eventId]);
-            setAdditionalDetails([...additionalDetails, '']);
-        }
-    };
+      const updatedAdditionalDetails = [...additionalDetails];
+      updatedAdditionalDetails.splice(selectedIndex, 1);
+      setAdditionalDetails(updatedAdditionalDetails);
+    } else {
+      setSelectedEvents([...selectedEvents, eventId]);
+      setAdditionalDetails([...additionalDetails, ""]);
+    }
+  };
 
-    const handleAdditionalDetailsChange = (index, value) => {
-        const updatedAdditionalDetails = [...additionalDetails];
-        updatedAdditionalDetails[index] = value;
-        setAdditionalDetails(updatedAdditionalDetails);
-    };
+  const handleAdditionalDetailsChange = (index, value) => {
+    const updatedAdditionalDetails = [...additionalDetails];
+    updatedAdditionalDetails[index] = value;
+    setAdditionalDetails(updatedAdditionalDetails);
+  };
 
-    return (
-        <div>
-            <p className="text-4xl text-white font-bold">EventsCheckbox</p>
-            <div className="flex flex-wrap items-center justify-evenly border-2 w-2/3 h-full p-5">
-                {events.map((event, index) => (
-                    <div key={event.eventId}>
-                        <div
-                            className={`bg-black p-4 rounded-lg border-4 border-blue-500 ${selectedEvents.includes(event.eventId) ? 'bg-grey-500' : ''}`}
-                            onClick={() => handleEventCardClick(event.eventId)}
-                        >
-                            <h2 className="text-lg font-bold text-white">{event.eventName}</h2>
-                            <p className="text-lg font-bold text-white">Team Size: {event.teamSize}</p>
-                            <p className="text-lg font-bold text-white">Price: {event.priceMoney}</p>
-                            <p className="text-lg font-bold text-white">Entry Fee: {event.entryFee}</p>
-                            {selectedEvents.includes(event.eventId) && (
-                                <input
-                                    type="text"
-                                    placeholder="Additional Details"
-                                    value={additionalDetails[selectedEvents.indexOf(event.eventId)] || ''}
-                                    onChange={(e) => handleAdditionalDetailsChange(selectedEvents.indexOf(event.eventId), e.target.value)}
-                                    onClick={(e) => e.stopPropagation()}
-                                />
-                            )}
-                        </div>
-
-                    </div>
-                ))}
-                <div>
-                    <h2 className="text-white">Selected Events</h2>
-                    <ul>
-                        {selectedEvents.map((eventId, index) => (
-                            <li key={eventId} className="text-white">
-                                Event ID: {eventId}, Additional Details: {additionalDetails[index]}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-            <button
-                className="bg-orange-600 ml-56 rounded-md text-3xl px-6 py-3 items-center justify-center mt-10"
-                onClick={() => {
-                    try {
-                        fetch("http://technorollix.opju.ac.in:4000/api/team-registration/event", {
-                            method: "POST",
-                            body: JSON.stringify({
-                                eventId: selectedEvents,
-                                leader: leaderEmail,
-                                additionalDetails: additionalDetails,
-                                teammates: teammateEmails.filter(email => email.trim() !== ''),
-                            }),
-                            headers: {
-                                "Content-type": "application/json",
-                            },
-                        })
-                            .then(async (res) => {
-                                if (!res.ok) {
-                                    throw new Error(`HTTP error! Status: ${res.status}`);
-                                }
-                                window.location.href = `/`;
-                                const json = await res.json();
-                            })
-                            .catch((error) => {
-                                console.log("Error during fetch:", error);
-                            });
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }}
+  return (
+    <div>
+      <p className="text-4xl text-white font-bold">EventsCheckbox</p>
+      <div className="flex flex-wrap items-center justify-evenly border-2 w-2/3 h-full p-5">
+        {events.map((event, index) => (
+          <div key={event.eventId}>
+            <div
+              className={`bg-black p-4 rounded-lg border-4 border-blue-500 ${
+                selectedEvents.includes(event.eventId) ? "bg-grey-500" : ""
+              }`}
+              onClick={() => handleEventCardClick(event.eventId)}
             >
-                Submit
-            </button>
+              <h2 className="text-lg font-bold text-white">
+                {event.eventName}
+              </h2>
+              <p className="text-lg font-bold text-white">
+                Team Size: {event.teamSize}
+              </p>
+              <p className="text-lg font-bold text-white">
+                Price: {event.priceMoney}
+              </p>
+              <p className="text-lg font-bold text-white">
+                Entry Fee: {event.entryFee}
+              </p>
+              {selectedEvents.includes(event.eventId) && (
+                <input
+                  type="text"
+                  placeholder="Additional Details"
+                  value={
+                    additionalDetails[selectedEvents.indexOf(event.eventId)] ||
+                    ""
+                  }
+                  onChange={(e) =>
+                    handleAdditionalDetailsChange(
+                      selectedEvents.indexOf(event.eventId),
+                      e.target.value
+                    )
+                  }
+                  onClick={(e) => e.stopPropagation()}
+                />
+              )}
+            </div>
+          </div>
+        ))}
+        <div>
+          <h2 className="text-white">Selected Events</h2>
+          <ul>
+            {selectedEvents.map((eventId, index) => (
+              <li key={eventId} className="text-white">
+                Event ID: {eventId}, Additional Details:{" "}
+                {additionalDetails[index]}
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      </div>
+      <button
+        className="bg-orange-600 ml-56 rounded-md text-3xl px-6 py-3 items-center justify-center mt-10"
+        onClick={() => {
+          try {
+            fetch(
+              "http://technorollix.opju.ac.in:4000/api/team-registration/event",
+              {
+                method: "POST",
+                body: JSON.stringify({
+                  eventId: selectedEvents,
+                  leader: leaderEmail,
+                  additionalDetails: additionalDetails,
+                  teammates: teammateEmails.filter(
+                    (email) => email.trim() !== ""
+                  ),
+                }),
+                headers: {
+                  "Content-type": "application/json",
+                },
+              }
+            )
+              .then(async (res) => {
+                if (!res.ok) {
+                  throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                window.location.href = `/`;
+                const json = await res.json();
+              })
+              .catch((error) => {
+                console.log("Error during fetch:", error);
+              });
+          } catch (error) {
+            console.log(error);
+          }
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export default EventsRegistrationPage;
