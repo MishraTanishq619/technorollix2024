cd5136c3abfc66ab140d0b888f1d739b57bed2b2
Tanishq Mishra <mishratanishq619@gmail.com>
2024-02-24 18:32:26 +0530

    invitations

--- techno_frontend/src/app/registration/invitations/page.js
+++ techno_frontend/src/app/registration/invitations/page.js
@@ -1,17 +1,15 @@
-import { Suspense } from 'react';
+import { Suspense } from "react";
 
 import React from "react";
-import ReceivedInvitations from './eventRegistrationCard';
+import ReceivedInvitations from "./invitationsRecieved";
 
 function page() {
 	return (
 		<div className="w-full h-screen items-center justify-evenly ml-56 mt-12">
-			{/* <div className=" "> */}
-				{/* <EventsRegistrationPage/> */}
-				<Suspense fallback={<div>Loading...</div>}>
-					<ReceivedInvitations/>
-				</Suspense>
-			{/* </div> */}
+			{/* <EventsRegistrationPage/> */}
+			<Suspense fallback={<div>Loading...</div>}>
+				<ReceivedInvitations />
+			</Suspense>
 		</div>
 	);
 }
