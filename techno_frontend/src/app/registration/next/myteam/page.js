e2d58a1f5266f05fd0b8ebd67c4bfd923c017229
Tanishq Mishra <mishratanishq619@gmail.com>
2024-02-29 03:53:26 +0530

    my-team

--- techno_frontend/src/app/registration/next/myteam/page.js
+++ techno_frontend/src/app/registration/next/myteam/page.js
@@ -59,17 +59,64 @@ const page = () => {
         return data;
       })
       .catch((error) => console.error('Error fetching TeamId:', error));
+    window.location.reload();
+
     // console.log("response: ", response);
   };
 
-  const invalidInputEmailHandler = () => {
-    let status = document.getElementById('status');
-    status.innerText = 'Invalid Email.';
-    status.style.color = 'red';
+  const invalidInputEmailHandler = (index) => {
+    let inputEmail = document.getElementById(`inputEmail${index}`);
+
+    // alert(<AlertComponent />);
+    alertHandler();
   };
   return (
     <div className="w-full flex justify-center my-10">
       {/* {console.log(reqEvents)} */}
+      <div
+        id="alert-1"
+        className="hidden absolute z-[99]   items-center p-4 mb-4 text-red-600 rounded-lg bg-blue-50 dark:bg-red-800 dark:text-white"
+        role="alert"
+      >
+        <svg
+          className="flex-shrink-0 w-4 h-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span className="sr-only">Info</span>
+        <div className="ms-3 text-lg font-medium mx-2">
+          Invalid Email or Number of Members Exceeded.
+        </div>
+        <button
+          type="button"
+          className="ms-auto -mx-1.5 -my-1.5 bg-blue-50 border-2 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-red-500 inline-flex items-center justify-center h-6 w-6 dark:bg-red-900 dark:text-white "
+          data-dismiss-target="#alert-1"
+          aria-label="Close"
+          onClick={dismissAlert}
+        >
+          <span className="sr-only">Close</span>
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+      </div>
+      {/*  */}
       <div className="w-[90%]">
         <p className="text-4xl text-white flex font-bold  my-4">
           <p className="w-[100%] text-center md:ml-[10%]">Team Construction </p>
@@ -125,7 +172,7 @@ const page = () => {
                     placeholder="Email of partner"
                     className="p-2 rounded-sm"
                   />
-                  <p id={`status${index}`} className=""></p>
+                  <p id={`status${index}`} className="mt-3 mx-2"></p>
                 </div>
 
                 <button
@@ -147,8 +194,7 @@ const page = () => {
                           inputEm.data,
                           reqTeamsArray[index]
                         )
-                      : invalidInputEmailHandler();
-                    window.location.reload();
+                      : invalidInputEmailHandler(index);
                   }}
                 >
                   <span className="relative">Invite</span>
@@ -164,29 +210,63 @@ const page = () => {
 
 export default page;
 
-// const Component = ({ eventId, emailRef }) => {
-// 	useEffect(() => {
-// 		let response = "ahwbdhwafwa";
-// 		response = fetch(
-// 			"http://10.60.41.209:4000/api/event/invite/status/byInviter/eventId",
-// 			{
-// 				method: "POST",
-// 				body: JSON.stringify({
-// 					teamId: eventId,
-// 					inviterEmail: emailRef,
-// 				}),
-// 				headers: {
-// 					"Content-type": "application/json",
-// 				},
-// 			}
-// 		)
-// 			.then((response) => response.json())
-// 			.then((data) => {
-// 				// console.log(data);
-// 				return data;
-// 			})
-// 			.catch((error) => console.error("Error fetching TeamId:", error));
-// 		console.log("and response: ", response);
-// 	}, []);
-// 	return <div>Component</div>;
-// };
+const AlertComponent = () => {
+  return (
+    <div
+      id="alert-1"
+      class="flex items-center p-4 mb-4 text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 w-4 h-4"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+      </svg>
+      <span class="sr-only">Info</span>
+      <div class="ms-3 text-sm font-medium">
+        A simple info alert with an{' '}
+        <a href="#" class="font-semibold underline hover:no-underline">
+          example link
+        </a>
+        . Give it a click if you like.
+      </div>
+      <button
+        type="button"
+        class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
+        data-dismiss-target="#alert-1"
+        aria-label="Close"
+      >
+        <span class="sr-only">Close</span>
+        <svg
+          class="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+function alertHandler() {
+  let alertBox = document.getElementById('alert-1');
+  alertBox.style.display = 'flex';
+}
+
+function dismissAlert() {
+  let alertBox = document.getElementById('alert-1');
+  alertBox.style.display = 'none';
+}
