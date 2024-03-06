0db31e5e5af6154b0a2d06a6d93b6a9ae64e9747
Shrey-raj-singh <rjshrey23@gmail.com>
2024-03-06 16:16:32 +0530

    use client

--- techno_frontend/src/app/events/antaragni/page.js
+++ techno_frontend/src/app/events/antaragni/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import EventsDetails from '../detsEvents';
 import Header from '@/components/Header';
 import { BackgroundBeams } from '@/components/ui/background-beams';
