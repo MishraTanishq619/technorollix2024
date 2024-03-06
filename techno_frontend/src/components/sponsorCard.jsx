0242b52514bdab5c1f73334299b4c9561a1c8a72
Tanishq Mishra <mishratanishq619@gmail.com>
2024-03-06 15:55:05 +0530

    jsplLogo

--- techno_frontend/src/components/sponsorCard.jsx
+++ techno_frontend/src/components/sponsorCard.jsx
@@ -48,12 +48,14 @@ const SkeletonTwo = () => {
 const SkeletonThree = () => {
   return (
     <div>
-      <p className="font-bold text-4xl text-white">Greens all over</p>
+      <p className="font-bold text-4xl text-white">
+        Jindal Steel & Power Limited.
+      </p>
       <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+      {/* <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
         A house surrounded by greenery and nature&apos;s beauty. It&apos;s the
         perfect place to relax, unwind, and enjoy life.
-      </p>
+      </p> */}
     </div>
   );
 };
@@ -73,16 +75,16 @@ const SkeletonFour = () => {
 const cards = [
   {
     id: 1,
+    content: <SkeletonThree />,
+    className: 'col-span-1',
+    thumbnail: '/jsplLogo.png',
+  },
+  {
+    id: 2,
     content: <SkeletonFour />,
     className: 'col-span-1',
-    thumbnail:'/sponsorsPic/codeforit.png',
+    thumbnail: '/sponsorsPic/codeforit.png',
   },
-  // {
-  //   id: 2,
-  //   content: <SkeletonFour />,
-  //   className: 'col-span-1',
-  //   thumbnail:'/gallery/darshanRaval2.jpg',
-  // },
   // {
   //   id: 3,
   //   content: <SkeletonOne />,
