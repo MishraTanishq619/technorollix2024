6156e299d2a423650510e08c31f7df35aa2224c4
Tanishq Mishra <mishratanishq619@gmail.com>
2024-03-08 19:26:43 +0530

    schedule-done

--- techno_frontend/src/app/schedule/page.js
+++ techno_frontend/src/app/schedule/page.js
@@ -55,14 +55,14 @@ const page = () => {
                     }`}
                     // style="left: 50%; border: 2px solid #FFC100; border-radius: 1%;"
                   ></div>
-                  {data.map((i, K) => {
+                  {events.map((i, K) => {
                     return (
                       <ScheduleTimelineCards
                         key={i.id}
                         id={i.id}
-                        date={i.date}
-                        heading={i.heading}
-                        paragraph={i.paragraph}
+                        datetime={i.datetime}
+                        event_name={i.event_name}
+                        venue={i.venue}
                         WinWidthBool={WinWidth}
                       />
                     );
@@ -83,54 +83,210 @@ const page = () => {
 
 export default page;
 
-const data = [
+const events = [
   {
     id: 1,
-    date: '1-6 May, 2021',
-    heading: 'Registration',
-    paragraph:
-      'Pick your favourite event(s) and register in that event by filling the form corresponding to that event. Its that easy :',
+    datetime: '20th March, 10:00 AM',
+    venue: 'Babuji Chowk',
+    event_name: 'Opening Ceremony & Tech-Lab: Model Presentation',
   },
   {
     id: 2,
-    date: '1-6 May, 2021',
-    heading: 'Registration',
-    paragraph:
-      'Pick your favourite event(s) and register in that event by filling the form corresponding to that event. Its that easy :',
+    datetime: '20th March, 11:00 AM',
+    venue: 'Central Library',
+    event_name: 'KalaKriti (Exhibition, Colour Painting, and Bottle Painting)',
   },
   {
     id: 3,
-    date: '1-6 May, 2021',
-    heading: 'Registration',
-    paragraph:
-      'Pick your favourite event(s) and register in that event by filling the form corresponding to that event. Its that easy :',
+    datetime: '20th March, 11:30 AM',
+    venue: 'EE Seminar Hall (FB-14)',
+    event_name: 'Ideathon',
   },
   {
     id: 4,
-    date: '1-6 May, 2021',
-    heading: 'Registration',
-    paragraph:
-      'Pick your favourite event(s) and register in that event by filling the form corresponding to that event. Its that easy :',
+    datetime: '20th March, 11:30 AM',
+    venue: 'Cafeteria',
+    event_name: 'Master Chef (Round 1)',
   },
   {
     id: 5,
-    date: '1-6 May, 2021',
-    heading: 'Registration',
-    paragraph:
-      'Pick your favourite event(s) and register in that event by filling the form corresponding to that event. Its that easy :',
+    datetime: '20th March, 12:30 PM',
+    venue: 'TB-01',
+    event_name: 'Hackathon',
   },
   {
     id: 6,
-    date: '1-6 May, 2021',
-    heading: 'Registration',
-    paragraph:
-      'Pick your favourite event(s) and register in that event by filling the form corresponding to that event. Its that easy :',
+    datetime: '20th March, 1:30 PM',
+    venue: 'Bus Parking Area',
+    event_name: 'Roadies (Round 1)',
   },
   {
     id: 7,
-    date: '1-6 May, 2021',
-    heading: 'Registration',
-    paragraph:
-      'Pick your favourite event(s) and register in that event by filling the form corresponding to that event. Its that easy :',
+    datetime: '20th March, 1:30 PM',
+    venue: 'Mock CID University Campus',
+    event_name: 'Mock CID',
+  },
+  {
+    id: 8,
+    datetime: '20th March, 2:00 PM',
+    venue: 'MP Hall',
+    event_name: 'Brainy Escape',
+  },
+  {
+    id: 9,
+    datetime: '20th March, 2:30 PM',
+    venue: 'TB-09',
+    event_name: 'Codigo',
+  },
+  {
+    id: 10,
+    datetime: '20th March, 3:00 PM',
+    venue: 'Opposite Gym',
+    event_name: 'Nukkad Natak',
+  },
+  {
+    id: 11,
+    datetime: '20th March, 6:30 PM',
+    venue: 'University Playground',
+    event_name: 'Inaugural Function',
+  },
+  {
+    id: 12,
+    datetime: '20th March, 7:30 PM',
+    venue: 'University Playground',
+    event_name: 'Antaragni + Aaghaaz',
+  },
+  {
+    id: 13,
+    datetime: '21st March, 10:00 AM',
+    venue: 'Babuji Chowk',
+    event_name: 'Robo Soccer',
+  },
+  {
+    id: 14,
+    datetime: '21st March, 10:30 AM',
+    venue: 'EE Seminar Hall (FB-14)',
+    event_name: 'RJ Hunt (Round 1)',
+  },
+  {
+    id: 15,
+    datetime: '21st March, 11:00 AM',
+    venue: 'Library Lawn & opposite to Chemistry lab',
+    event_name: 'KalaKriti (Decoration, Photorollix & Rangoli)',
+  },
+  {
+    id: 16,
+    datetime: '21st March, 11:00 AM',
+    venue: 'Cafeteria',
+    event_name: 'Master Chef (Finale)',
+  },
+  {
+    id: 17,
+    datetime: '21st March, 11:30 AM',
+    venue: 'TB-09',
+    event_name: 'Codigo',
+  },
+  {
+    id: 18,
+    datetime: '21st March, 11:30 AM',
+    venue: 'MP Hall',
+    event_name: 'Yuva Sabha (Round 1)',
+  },
+  {
+    id: 19,
+    datetime: '21st March, 12:00 PM',
+    venue: 'TB-04,05,06,07',
+    event_name: 'Game Fusion',
+  },
+  {
+    id: 20,
+    datetime: '21st March, 2:00 PM',
+    venue: 'Treasure Hunt Opposite to CDC office garden',
+    event_name: 'Treasure Hunt',
+  },
+  {
+    id: 21,
+    datetime: '21st March, 2:00 PM',
+    venue: 'Bus Parking Area',
+    event_name: 'Roadies (Round 2)',
+  },
+  {
+    id: 22,
+    datetime: '21st March, 2:30 PM',
+    venue: 'Live Classroom',
+    event_name: 'Start-up Business Plan',
+  },
+  {
+    id: 23,
+    datetime: '21st March, 2:30 PM',
+    venue: 'G-08 (Modeling & Simulation Lab)',
+    event_name: 'Brainy Escape',
+  },
+  {
+    id: 24,
+    datetime: '21st March, 3:00 PM',
+    venue: 'Volleyball Court',
+    event_name: 'Robo Race',
+  },
+  {
+    id: 25,
+    datetime: '21st March, 6:30 PM',
+    venue: 'University Playground',
+    event_name: 'Antaragni',
+  },
+  {
+    id: 26,
+    datetime: '22nd March, 10:00 AM',
+    venue: 'Babuji Chowk',
+    event_name: 'Robo War',
+  },
+  {
+    id: 27,
+    datetime: '22nd March, 10:00 AM',
+    venue: 'MP Hall',
+    event_name: 'Talent Hunt',
+  },
+  {
+    id: 28,
+    datetime: '22nd March, 10:30 AM',
+    venue: 'EE Seminar Hall (FB-14)',
+    event_name: 'RJ Hunt (Finale)',
+  },
+  {
+    id: 29,
+    datetime: '22nd March, 11:00 AM',
+    venue: 'Bus Parking Area',
+    event_name: 'Roadies (Round 3)',
+  },
+  {
+    id: 30,
+    datetime: '22nd March, 12:00 PM',
+    venue: 'TB-04,05,06,07',
+    event_name: 'Game Fusion',
+  },
+  {
+    id: 31,
+    datetime: '22nd March, 1:30 PM',
+    venue: 'MP Hall',
+    event_name: 'Yuva Sabha (Finale)',
+  },
+  {
+    id: 32,
+    datetime: '22nd March, 4:00 PM',
+    venue: 'Babuji Chowk',
+    event_name: 'Aero Drone',
+  },
+  {
+    id: 33,
+    datetime: '22nd March, 6:30 PM',
+    venue: 'OAT',
+    event_name:
+      'Valedictory Function, Prize Distribution, Sports, Academic and Annual Awards',
+  },
+  {
+    id: 34,
+    datetime: '22nd March, 8:00 PM',
+    venue: 'University Playground',
+    event_name: 'JUnoon, Celebrity Night by Pawandeep & Arunita',
   },
 ];
